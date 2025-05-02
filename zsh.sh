e06b669a2ecacbbd82700c08797ea6ab22968d4d
khnningr <barrios12khnningr@proton.me>
2025-05-01 20:27:17 -0600

    Creación del repositorio.; creación de los dots.: 2025/05/01 a las 08:27 p.m.

---
+++ zsh.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+DIR_DOTFILES="$HOME/hyprcrow/"
+
+# Verifica si existe el directorio, saliendo del scrip,
+# si no existe.
+[ -d "$DIR_DOTFILES" ] || {
+  exit 1
+}
+
+# Paquetes necesarios.
+zen=(
+  "zsh"
+  "vivaldi"
+
+
+  )
+
+sudo pacman -S --needed --noconfirm "${zen[@]}" 
+
+# Stow
+cd "$HOME"
+mv .zshrc .zshrc.bak
+cd "$DIR_DOTFILES" 
+stow 
+# Definir el Shell.
+# chsh <USUARIO> - este comando permite cambiar el Shell.
+# para ejemplo: /usr/bin/bash o /usr/bin/zsh
+# echo $SHELL - para ver el Shell que se está usando.
+
+
+
+
